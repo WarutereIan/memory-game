f6fddb701b242b39c4dbc1a536bfc4f58319a21f
ian.dev <nmwanik111@gmail.com>
2024-12-21 02:47:47 +0300

    docker updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,12 +7,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
-  define: {
+  /*   define: {
     VITE_API_URL: JSON.stringify(
       process.env.VITE_API_URL || "VITE_API_URL_PLACEHOLDER"
     ),
     VITE_SOCKET_URL: JSON.stringify(
       process.env.VITE_SOCKET_URL || "VITE_SOCKET_URL_PLACEHOLDER"
     ),
-  },
+  }, */
 });
