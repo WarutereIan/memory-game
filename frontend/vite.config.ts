3aa141069c072465b198e0f02025931b043846a7
ian.dev <nmwanik111@gmail.com>
2024-12-21 00:52:42 +0300

    added docker configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
+  },
+  define: {
+    VITE_API_URL: JSON.stringify(
+      process.env.VITE_API_URL || "VITE_API_URL_PLACEHOLDER"
+    ),
+    VITE_SOCKET_URL: JSON.stringify(
+      process.env.VITE_SOCKET_URL || "VITE_SOCKET_URL_PLACEHOLDER"
+    ),
   },
 });
